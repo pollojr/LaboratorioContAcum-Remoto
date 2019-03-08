541e9c9e152a410398a3e867a27e956efb832ca3
Maicol Esneider Bernal Pena <000404194@bga.upb>
2019-03-08 12:48:03 -0500

    mi primer commit

---
+++ primos.py
@@ -0,0 +1,6 @@
+a=int(input("digite un numero"))
+b=int(input("digite otro numero"))
+if a%b == 0:
+    print(" el numero es divisible entre b")
+else:
+        print("el numero no es divisible entre b")
